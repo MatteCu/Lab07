798d82519be248cbf8bc8245118a2906ba08408d
utente <s296464@studenti.polito.it>
2025-04-14 23:06:36 +0200

    appunti a lezione

--- database/meteo_dao.py
+++ database/meteo_dao.py
@@ -1,7 +1,11 @@
 from database.DB_connect import DBConnect
 from model.situazione import Situazione
 
-
+''' note per il database: il database è "sporco", ad esempio, nel mese di gennaio manca
+    il dato per il primo giorno. Devo scrivere il codice in funzione delle possibili
+    complicazioni che possono accadere nei diversi database, es dati corrotti, linee non
+    leggibili. Sarebbe utile impostare una linea di default per superare questi errori
+'''
 class MeteoDao():
 
     @staticmethod
--- model/model.py
+++ model/model.py
@@ -1,3 +1,46 @@
+
+
+from database.meteo_dao import MeteoDao
 class Model:
     def __init__(self):
         pass
+    def calcola_sequenza(self, mese):
+        situazioni = MeteoDao.get_situazioni_meta_mese(mese)
+
+    #parte di soluzione con la ricorsione!
+    def _ricorsione(self, parziale, liata_situazioni):
+
+        #condizione terminale
+        # NB la lunghezza del parziale indica il giorno di cui cerco le soulzioni.
+
+        if (len(parziale))==15:
+            print(parziale)
+
+        #condizione ricorsiva
+        else:
+            #devo cercare le città per il girno che mi serve
+            #provo ad aggiungere una di queste citta e vado avanti.
+
+            '''
+            [1:MILANO][2]
+            [][][][][][][][] ... [] : 44 dati
+            '''
+            #candidati=trova_possibili_step(self, parziale, liata_situazioni)
+
+            for candidato in candidati:
+                parziale.append(candidato)
+                self._ricorsione(parziale,liata_situazioni)
+                parziale.pop()
+                #nota bene, dato che ogni giorno ha un numero limite di soluzioni (es il giorno uno
+                #una sola e così via, posso fare dei check per limitare le iterazioni!
+
+#per usare la ricorsione, tengo un codice in ricorsione pulito, e imposto all'esterno i vincoli!
+
+#esempio di vincolo:
+#[T][T][T][M][M][M][M][G][G][G]
+"""se gli ultimi tre non sono uguali, il prossimo step dell'algoritmo deve inserire per forza
+    una città uguale all'ultima soluzione,
+    Osservazione: in pratica devo generare tutte le sequenze di viaggio ammissibili, escludendo quelle
+    impossibili per limitare le iterazioni con i vincoli assegnati.
+    In seguito, calcolo il costo per ogni sequenza per trovare la sequenza ottima.
+"""
